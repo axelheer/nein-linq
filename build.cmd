c1318baed4a8a3a3986f28e605867b75c564b50c
Axel Heer <axel@heer.eu>
2016-06-15 21:18:18 +0200

    Enable AppVeyor

--- build.cmd
+++ build.cmd
@@ -4,6 +4,9 @@ dotnet restore
 
 dotnet build src\*\project.json --configuration Debug
 dotnet build src\*\project.json --configuration Release
-
 dotnet build test\*\project.json --configuration Debug
 dotnet build test\*\project.json --configuration Release
+
+dotnet pack src\NeinLinq --configuration Release
+dotnet pack src\NeinLinq.EF6 --configuration Release
+dotnet pack src\NeinLinq.EFCore --configuration Release
--- pack.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-dotnet restore
-
-dotnet pack src\NeinLinq --configuration Release
-dotnet pack src\NeinLinq.EF6 --configuration Release
-dotnet pack src\NeinLinq.EFCore --configuration Release
