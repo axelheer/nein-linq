ac7c5fecec59fffa0d6f5c617b994176ff8a049d
Axel Heer <axel@heer.eu>
2016-06-22 22:04:24 +0200

    Improve tests

--- test.cmd
+++ test.cmd
@@ -5,8 +5,8 @@ set reportgenerator=%UserProfile%\.nuget\packages\ReportGenerator\2.4.5\tools\Re
 
 if not exist TestResults mkdir TestResults
 
-"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinLinq.Tests --configuration Release -xml TestResults\NeinLinq.result.xml" -output:TestResults\NeinLinq.report.xml -register:user -filter:+[NeinLinq]*
-"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinLinq.Tests.EF6 --configuration Release -xml TestResults\NeinLinq.EF6.result.xml" -output:TestResults\NeinLinq.EF6.report.xml -register:user -filter:+[NeinLinq.EF6]*
-"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinLinq.Tests.EFCore --configuration Release -xml TestResults\NeinLinq.EFCore.result.xml" -output:TestResults\NeinLinq.EFCore.report.xml -register:user -filter:+[NeinLinq.EFCore]*
+"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinLinq.Tests -xml TestResults\NeinLinq.result.xml" -output:TestResults\NeinLinq.report.xml -register:user -filter:+[NeinLinq]*
+"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinLinq.Tests.EF6 -xml TestResults\NeinLinq.EF6.result.xml" -output:TestResults\NeinLinq.EF6.report.xml -register:user -filter:+[NeinLinq.EF6]*
+"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinLinq.Tests.EFCore -xml TestResults\NeinLinq.EFCore.result.xml" -output:TestResults\NeinLinq.EFCore.report.xml -register:user -filter:+[NeinLinq.EFCore]*
 
 "%reportgenerator%" -reports:TestResults\NeinLinq.report.xml;TestResults\NeinLinq.EF6.report.xml;TestResults\NeinLinq.EFCore.report.xml -targetdir:TestResults\report -reporttypes:Badges;Html
