0e8bcae2fea2cf2d5c9966a035989b7ae3ca96e8
Axel Heer <axel@heer.eu>
2016-06-22 21:52:36 +0200

    Waste some time

--- init.cmd
+++ init.cmd
@@ -1,5 +1,9 @@
 @echo off
 
-set build_options=--configuration Release --version-suffix yolo
-if defined appveyor_build_number set build_options=--configuration Release --version-suffix ci%appveyor_build_number%
-if /i [%appveyor_repo_tag%]==[true] set build_options=--configuration Release
+if [%appveyor_repo_branch%]==[release] (
+	set build_options=--configuration Release
+) else if defined appveyor_build_number (
+	set build_options=--configuration Release --version-suffix ci%appveyor_build_number%
+) else (
+	set build_options=--configuration Release --version-suffix yolo
+)
