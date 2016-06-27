b71bc04bbb153b9833c29d834da18db89d4814f6
Axel Heer <axel@heer.eu>
2016-06-27 22:48:04 +0200

    Purge tabs

--- init.cmd
+++ init.cmd
@@ -1,9 +1,9 @@
 @echo off
 
 if [%appveyor_repo_branch%]==[release] (
-	set build_options=--configuration Release
+  set build_options=--configuration Release
 ) else if defined appveyor_build_number (
-	set build_options=--configuration Release --version-suffix ci%appveyor_build_number%
+  set build_options=--configuration Release --version-suffix ci%appveyor_build_number%
 ) else (
-	set build_options=--configuration Release --version-suffix yolo
+  set build_options=--configuration Release --version-suffix yolo
 )
