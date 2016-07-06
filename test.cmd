7d3a5b604aceb1a1663a67f11d0d6658a4dcae01
Axel Heer <axel@heer.eu>
2016-07-06 21:39:16 +0200

    Run tests separately

--- test.cmd
+++ test.cmd
@@ -5,8 +5,10 @@ set reportgenerator=%UserProfile%\.nuget\packages\ReportGenerator\2.4.5\tools\Re
 
 if not exist TestResults mkdir TestResults || goto :eof
 
-"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinLinq.Tests -xml TestResults\NeinLinq.result.xml" -output:TestResults\NeinLinq.report.xml -register:user -filter:+[NeinLinq]* -returntargetcode || goto :eof
-"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinLinq.Tests.EF6 -xml TestResults\NeinLinq.EF6.result.xml" -output:TestResults\NeinLinq.EF6.report.xml -register:user -filter:+[NeinLinq.EF6]* -returntargetcode || goto :eof
-"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinLinq.Tests.EFCore -xml TestResults\NeinLinq.EFCore.result.xml" -output:TestResults\NeinLinq.EFCore.report.xml -register:user -filter:+[NeinLinq.EFCore]* -returntargetcode || goto :eof
+"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinLinq.Tests --configuration Release --framework net461 -xml TestResults\NeinLinq.netframework.result.xml" -output:TestResults\NeinLinq.netframework.report.xml -register:user -filter:+[NeinLinq]* -returntargetcode || goto :eof
+"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinLinq.Tests --configuration Release --framework netcoreapp1.0 -xml TestResults\NeinLinq.netcoreapp.result.xml" -output:TestResults\NeinLinq.netcoreapp.report.xml -register:user -filter:+[NeinLinq]* -returntargetcode || goto :eof
+"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinLinq.Tests.EF6 --configuration Release --framework net461 -xml TestResults\NeinLinq.EF6.netframework.result.xml" -output:TestResults\NeinLinq.EF6.netframework.report.xml -register:user -filter:+[NeinLinq.EF6]* -returntargetcode || goto :eof
+"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinLinq.Tests.EFCore --configuration Release --framework net461 -xml TestResults\NeinLinq.EFCore.netframework.result.xml" -output:TestResults\NeinLinq.EFCore.netframework.report.xml -register:user -filter:+[NeinLinq.EFCore]* -returntargetcode || goto :eof
+"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinLinq.Tests.EFCore --configuration Release --framework netcoreapp1.0 -xml TestResults\NeinLinq.EFCore.netcoreapp.result.xml" -output:TestResults\NeinLinq.EFCore.netcoreapp.report.xml -register:user -filter:+[NeinLinq.EFCore]* -returntargetcode || goto :eof
 
-"%reportgenerator%" -reports:TestResults\NeinLinq.report.xml;TestResults\NeinLinq.EF6.report.xml;TestResults\NeinLinq.EFCore.report.xml -targetdir:TestResults\report -reporttypes:Badges;Html || goto :eof
+"%reportgenerator%" -reports:TestResults\*.report.xml -targetdir:TestResults\report -reporttypes:Badges;Html || goto :eof
