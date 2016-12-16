6d4d57b4a07ed4b0993c8601faf4bda7d1cb648a
Axel Heer <axel@heer.eu>
2016-12-16 19:28:56 +0100

    Set version-suffix to rc1

--- init.cmd
+++ init.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 if [%appveyor_repo_branch%]==[release] (
-  set build_options=--configuration Release
+  set build_options=--configuration Release --version-suffix rc1
 ) else if defined appveyor_build_number (
   set build_options=--configuration Release --version-suffix ci%appveyor_build_number%
 ) else (
