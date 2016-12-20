4b40e65acc2daf2dc0bb9e0caf679ce5363aaa70
Axel Heer <axel@heer.eu>
2016-12-20 19:29:44 +0100

    Update version-suffix

--- init.cmd
+++ init.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 if [%appveyor_repo_branch%]==[release] (
-  set build_options=--configuration Release --version-suffix rc2
+  set build_options=--configuration Release --version-suffix rc3
 ) else if defined appveyor_build_number (
   set build_options=--configuration Release --version-suffix ci%appveyor_build_number%
 ) else (
