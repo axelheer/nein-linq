bc44d04940c7ea80f241ddc20930f3f4e05d8e1c
Axel Heer <axel@heer.eu>
2016-12-31 10:17:25 +0100

    Fix code coverage

--- test.cmd
+++ test.cmd
@@ -1,11 +1,11 @@
 @echo off
 
 set opencover=%UserProfile%\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe
-set reportgenerator=%UserProfile%\.nuget\packages\ReportGenerator\2.5.1\tools\ReportGenerator.exe
+set reportgenerator=%UserProfile%\.nuget\packages\ReportGenerator\2.5.2\tools\ReportGenerator.exe
 
 if not exist TestResults mkdir TestResults || goto :eof
 
-"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinLinq.Tests --configuration Release --framework net461 -xml TestResults\NeinLinq.netframework.result.xml" -output:TestResults\NeinLinq.netframework.report.xml -register:user -filter:"+[NeinLinq*]* -[NeinLinq.Fakes]* -[NeinLinq.Tests]*" -returntargetcode || goto :eof
-"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinLinq.Tests --configuration Release --framework netcoreapp1.0 -xml TestResults\NeinLinq.netcoreapp.result.xml" -output:TestResults\NeinLinq.netcoreapp.report.xml -register:user -filter:"+[NeinLinq*]* -[NeinLinq.Fakes]* -[NeinLinq.Tests]*" -returntargetcode || goto :eof
+"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinLinq.Tests --configuration Release --framework net461 -xml TestResults\NeinLinq.netframework.result.xml" -output:TestResults\NeinLinq.netframework.report.xml -register:user -filter:"+[NeinLinq*]* -[NeinLinq.Fakes]* -[NeinLinq.Tests]*" -returntargetcode -oldstyle || goto :eof
+"%opencover%" -target:dotnet.exe -targetargs:"test test\NeinLinq.Tests --configuration Release --framework netcoreapp1.1 -xml TestResults\NeinLinq.netcoreapp.result.xml" -output:TestResults\NeinLinq.netcoreapp.report.xml -register:user -filter:"+[NeinLinq*]* -[NeinLinq.Fakes]* -[NeinLinq.Tests]*" -returntargetcode -oldstyle || goto :eof
 
 "%reportgenerator%" -reports:TestResults\*.report.xml -targetdir:TestResults\report -reporttypes:Badges;Html || goto :eof
