f213e58320db1a47ed0554d4b1f111f0f87b9237
Axel Heer <axel@heer.eu>
2017-01-01 17:38:22 +0100

    Go to 2017

--- src/NeinLinq.Interactive/Properties/AssemblyInfo.cs
+++ src/NeinLinq.Interactive/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Axel Heer")]
 [assembly: AssemblyProduct("NeinLinq")]
-[assembly: AssemblyCopyright("Copyright © 2014-2016 Axel Heer")]
+[assembly: AssemblyCopyright("Copyright © 2014-2017 Axel Heer")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
