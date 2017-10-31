8b451430c39bc29fa19b4fe238dcbfc71a847371
Axel Heer <axel@heer.io>
2017-10-31 21:38:05 +0100

    Write some tests

---
+++ test/NeinLinq.Tests/ParameterBinderTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace NeinLinq.Tests
+{
+    public class ParameterBinderTest
+    {
+        [Fact]
+        public void ShouldHandleInvalidArguments()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ParameterBinder(null, Expression.Constant(true)));
+            Assert.Throws<ArgumentNullException>(() => new ParameterBinder(Expression.Parameter(typeof(bool)), null));
+        }
+
+        [Fact]
+        public void ShouldVisitParameter()
+        {
+            var parameter = Expression.Parameter(typeof(bool));
+            var replacement = Expression.Constant(true);
+
+            var result = new ParameterBinder(parameter, replacement).Visit(parameter);
+
+            Assert.Equal(replacement, result);
+        }
+
+        [Fact]
+        public void ShouldNotVisitParameter()
+        {
+            var parameter = Expression.Parameter(typeof(bool));
+            var replacement = Expression.Constant(true);
+
+            var result = new ParameterBinder(Expression.Parameter(typeof(bool)), replacement).Visit(parameter);
+
+            Assert.Equal(parameter, result);
+        }
+
+        [Fact]
+        public void ShouldVisitInvocation()
+        {
+            var parameter = Expression.Parameter(typeof(Func<bool, bool>));
+            var replacement = Expression.Lambda<Func<bool, bool>>(Expression.Constant(true), Expression.Parameter(typeof(bool)));
+
+            var result = new ParameterBinder(parameter, replacement).Visit(Expression.Invoke(parameter, Expression.Parameter(typeof(bool))));
+
+            Assert.Equal(ExpressionType.Constant, result.NodeType);
+        }
+
+        [Fact]
+        public void ShouldNotVisitInvocation()
+        {
+            var parameter = Expression.Parameter(typeof(Func<bool, bool>));
+            var replacement = Expression.Lambda<Func<bool, bool>>(Expression.Constant(true), Expression.Parameter(typeof(bool)));
+
+            var result = new ParameterBinder(Expression.Parameter(typeof(bool)), replacement).Visit(Expression.Invoke(parameter, Expression.Parameter(typeof(bool))));
+
+            Assert.NotEqual(ExpressionType.Constant, result.NodeType);
+        }
+
+        [Fact]
+        public void ShouldNotVisitInvocationLambda()
+        {
+            var parameter = Expression.Parameter(typeof(Func<bool, bool>));
+            var replacement = Expression.Parameter(typeof(Func<bool, bool>));
+
+            var result = new ParameterBinder(parameter, replacement).Visit(Expression.Invoke(parameter, Expression.Parameter(typeof(bool))));
+
+            Assert.NotEqual(ExpressionType.Constant, result.NodeType);
+        }
+    }
+}
--- test/NeinLinq.Tests/SelectorTranslator/ToTest.cs
+++ test/NeinLinq.Tests/SelectorTranslator/ToTest.cs
@@ -26,6 +26,20 @@ namespace NeinLinq.Tests.SelectorTranslator
         }
 
         [Fact]
+        public void SubtypeShouldSubstituteWithoutValue()
+        {
+            Expression<Func<Dummy, DummyView>> s = d => new DummyView { Id = d.Id, Name = d.Name };
+
+            var select = s.Translate().To<SpecialDummy, SpecialDummyView>();
+            var result = data.OfType<SpecialDummy>().Select(select);
+
+            Assert.Collection(result,
+                v => { Assert.Equal(4, v.Id); Assert.Equal("Asdf", v.Name); Assert.Null(v.Description); },
+                v => { Assert.Equal(5, v.Id); Assert.Equal("Narf", v.Name); Assert.Null(v.Description); },
+                v => { Assert.Equal(6, v.Id); Assert.Equal("Qwer", v.Name); Assert.Null(v.Description); });
+        }
+
+        [Fact]
         public void PathShouldSubstitute()
         {
             Expression<Func<ParentDummy, ParentDummyView>> s = d => new ParentDummyView { Id = d.Id, Name = d.Name };
@@ -41,6 +55,28 @@ namespace NeinLinq.Tests.SelectorTranslator
         }
 
         [Fact]
+        public void PathShouldSubstituteWithoutValue()
+        {
+            Expression<Func<ParentDummy, ParentDummyView>> s = d => new ParentDummyView { Id = d.Id, Name = d.Name };
+
+            var select = s.Translate().To<ChildDummy, ChildDummyView>(d => d.Parent, d => d.Parent);
+            var result = data.OfType<ChildDummy>().Select(select);
+
+            Assert.Collection(result,
+                v => { Assert.Equal(0, v.Id); Assert.Null(v.Name); Assert.Equal(8, v.Parent.Id); Assert.Equal("Narf", v.Parent.Name); },
+                v => { Assert.Equal(0, v.Id); Assert.Null(v.Name); Assert.Equal(9, v.Parent.Id); Assert.Equal("Qwer", v.Parent.Name); },
+                v => { Assert.Equal(0, v.Id); Assert.Null(v.Name); Assert.Equal(7, v.Parent.Id); Assert.Equal("Asdf", v.Parent.Name); });
+        }
+
+        [Fact]
+        public void TranslationShouldHandleInvalidArguments()
+        {
+            Expression<Func<ChildDummy, ChildDummyView>> s = d => new ChildDummyView { Id = d.Id, Name = d.Name };
+
+            Assert.Throws<ArgumentNullException>(() => s.Translate().To(default(Expression<Func<ParentDummy, Func<ChildDummy, ChildDummyView>, ParentDummyView>>)));
+        }
+
+        [Fact]
         public void TranslationShouldSubstitute()
         {
             Expression<Func<ChildDummy, ChildDummyView>> s = d => new ChildDummyView { Id = d.Id, Name = d.Name };
@@ -53,6 +89,20 @@ namespace NeinLinq.Tests.SelectorTranslator
                 v => { Assert.Equal(7, v.Id); Assert.Equal("Asdf", v.Name); Assert.Equal(10, v.FirstChild.Id); Assert.Equal("Asdf", v.FirstChild.Name); },
                 v => { Assert.Equal(8, v.Id); Assert.Equal("Narf", v.Name); Assert.Equal(11, v.FirstChild.Id); Assert.Equal("Narf", v.FirstChild.Name); },
                 v => { Assert.Equal(9, v.Id); Assert.Equal("Qwer", v.Name); Assert.Equal(12, v.FirstChild.Id); Assert.Equal("Qwer", v.FirstChild.Name); });
+        }
+
+        [Fact]
+        public void TranslationShouldSubstituteWithoutValue()
+        {
+            Expression<Func<ChildDummy, ChildDummyView>> s = d => new ChildDummyView { Id = d.Id, Name = d.Name };
+
+            var select = s.Translate().To<ParentDummy, ParentDummyView>((d, v) => new ParentDummyView { FirstChild = d.Children.Select(v).First() });
+            var result = data.OfType<ParentDummy>().Select(select);
+
+            Assert.Collection(result,
+                v => { Assert.Equal(0, v.Id); Assert.Null(v.Name); Assert.Equal(10, v.FirstChild.Id); Assert.Equal("Asdf", v.FirstChild.Name); },
+                v => { Assert.Equal(0, v.Id); Assert.Null(v.Name); Assert.Equal(11, v.FirstChild.Id); Assert.Equal("Narf", v.FirstChild.Name); },
+                v => { Assert.Equal(0, v.Id); Assert.Null(v.Name); Assert.Equal(12, v.FirstChild.Id); Assert.Equal("Qwer", v.FirstChild.Name); });
         }
     }
 }
